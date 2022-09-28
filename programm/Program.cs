1f20eafba0d2a5a19e3836f1fb9d69dd2c835c2d
Лавренов Евгений <lavr020775@gmail.com>
2022-09-29 00:19:09 +0300

    edit programm

--- programm/Program.cs
+++ programm/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿Console.Write("Введите количество массивов:");
+int massivLength= Convert.ToInt32(Console.ReadLine());
+
+string[] str=new string[massivLength];
+for (int i =0; i < massivLength; i++)
+     {
+     Console.Write((i+1)+" массив=");
+     str[i]=Console.ReadLine();
+     }
+
+
+Console.Write(PrintMassiv(str));
+
+string PrintMassiv(string[] massiv)     
+{
+string str = "";    
+for (int i =0; i < massiv.Length; i++)
+    if (massiv[i].Length <= 3)
+        str=str+massiv[i]+" ";
+
+return str;        
+}
+
